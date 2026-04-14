396eee616763983b28271c840c03a3db7b667d12
Richard Perez <4702185+ripleyXLR8@users.noreply.github.com>
2026-04-15 00:01:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-# VERSION : 2.6.0
+# VERSION : 2.6.1
 # DATE    : 2026-04-14
 # DESCRIPTION : Refactoring - Fix Initialisation Gunicorn (Translations & Patcher) + Type Hinting + Blueprints
 
@@ -15,7 +15,7 @@ from modules.patcher import patcher_scheduler
 from routes.web import web_bp
 from routes.api import api_bp
 
-APP_VERSION: str = "2.6.0"
+APP_VERSION: str = "2.6.1"
 
 app = Flask(__name__)
 # Génère une clé sécurisée ou utilise la clé persistante générée au premier démarrage
